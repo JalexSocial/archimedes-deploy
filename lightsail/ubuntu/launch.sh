0b892d9474ed64692680b224ce1bac2f30e9160b
Michael Tanczos <mike@logic-gate.com>
2023-01-15 23:50:40 -0500

    Updated launch scripts

--- lightsail/ubuntu/launch.sh
+++ lightsail/ubuntu/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# NOTE: After initialization make sure to click the networking tab and open up an https port
+# NOTE: After initialization make sure to click the networking tab in Amazon Lightsail and open up an https port
 
 # Update package list and upgrade all packages
 apt update -y
@@ -20,12 +20,14 @@ apt-get update && \
 
 mkdir /var/archimedes  
 cd /var/archimedes
+chown ubuntu:ubuntu /var/archimedes
 git clone https://github.com/RoverLink/archimedes-deploy  
 
 cp /var/archimedes/archimedes-deploy/lightsail/ubuntu/scripts/services/archimedes.service /etc/systemd/system/
 cp /var/archimedes/archimedes-deploy/lightsail/ubuntu/scripts/nginx/sites-available/* /etc/nginx/sites-available
 
 ln -s /etc/nginx/sites-available/archimedes /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
 
 systemctl enable archimedes
 systemctl enable nginx
@@ -34,6 +36,8 @@ mkdir /var/www/archimedes
 chown ubuntu /var/www/archimedes
 
 chmod 755 /var/archimedes/archimedes-deploy/lightsail/ubuntu/scripts/certbot/install-certbot.sh
+
+certbot --nginx --non-interactive --agree-tos --redirect -m mike@logic-gate.com -d archimedes.jalex.io
 
 # Enable firewall
 apt-get install ufw
--- lightsail/ubuntu/scripts/certbot/install-certbot.sh
+++ lightsail/ubuntu/scripts/certbot/install-certbot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-certbot --nginx --non-interactive --agree-tos --redirect -m mike@logic-gate.com -d INSERT_HOSTNAME_HERE
+certbot --nginx --non-interactive --agree-tos --redirect -m mike@logic-gate.com -d archimedes.jalex.io
 
